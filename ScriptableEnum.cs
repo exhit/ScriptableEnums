c11d7da42edd790cdb4882859b430538c332416f
Pedro Martins <34919529+exhit@users.noreply.github.com>
2025-03-22 09:35:11 +0000

    DisableAttribute

--- ScriptableEnum.cs
+++ ScriptableEnum.cs
@@ -515,4 +515,42 @@ namespace Tauntastic.ScriptableEnums
         }
     }
 #endif
+    
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    [Preserve]
+    public class DisableAttribute : PropertyAttribute
+    {
+    }
+
+    public interface IDisableable
+    {
+        void Disable();
+    }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(DisableAttribute), true)]
+    public class DisableAttributePropertyDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var propertyField = new PropertyField(property);
+
+            propertyField.RegisterCallbackOnce<GeometryChangedEvent>(_ =>
+                {
+                    var firstChild = propertyField.Q();
+                    if (firstChild is IDisableable disableable)
+                    {
+                        disableable.Disable();
+                    }
+                    else
+                    {
+                        propertyField.SetEnabled(false);
+                    }
+                }
+            );
+
+            return propertyField;
+        }
+    }
+#endif
 }
